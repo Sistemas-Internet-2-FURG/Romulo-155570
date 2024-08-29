26d4487c0b3981bc64b36d80c2af74505bba3698
Romulo <romulotg12@gmail.com>
2024-08-29 00:57:54 -0300

    feat: adding User model on models.py

--- app/models.py
+++ app/models.py
@@ -1,4 +1,12 @@
 from app import db
+from flask_login import UserMixin
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), unique=True, nullable=False)
+    senha = db.Column(db.String(60), nullable=False)
+    role = db.Column(db.String(10), nullable=False)  # 'admin' or 'professor'
 
 class Turma(db.Model):
     id = db.Column(db.Integer, primary_key=True)
